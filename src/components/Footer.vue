f6dc12634b28383f7615714d4d3d70baa50a28e3
Your Name <you@example.com>
2022-07-30 16:30:38 +0200

    last

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full lg:h-[590px] ph:h-auto bg-violet-700">
       <div class="w-[90%] translate-y-[-90px] mx-auto ph:justify-center rounded-xl lg:h-[180px] ph:h-[350px] flex flex-row ph:flex-wrap justify-between items-center p-4 bg-gray-900">
-          <div class="lg:w-[25%] ph:w-[80%] text-center ph:my-2 h-[50px] font-bold text-4xl text-white">Start a project</div>
-          <div class="lg:w-[30%] ph:w-[80%] text-center ph:my-2 text-white h-[50px] text-xl">Interested in working together? We should queue up a chat. I’ll buy the coffee.</div>
+          <div class="lg:w-[25%] ph:w-[80%] text-center ph:my-2 h-[50px] font-bold ph:text-2xl lg:text-4xl text-white">Start a project</div>
+          <div class="lg:w-[30%] ph:w-[80%] text-center ph:my-2 text-white h-[50px]  ph:text-lg lg:text-xl">Interested in working together? We should queue up a chat. I’ll buy the coffee.</div>
           <div class="outline ph:w-[80%] ph:my-2 text-center border-none outline-violet-700 hover:bg-violet-700 duration-500 rounded-full text-gray-50 h-[50px] flex justify-center items-center cursor-pointer lg:w-[20%]"><div class="text-xl font-bold"><a href="https://khamsat.com/programming/css-html/1995166-%D8%AA%D8%B5%D9%85%D9%8A%D9%85-%D9%85%D9%88%D9%82%D8%B9-%D8%A7%D8%AD%D8%AA%D8%B1%D8%A7%D9%81%D9%89-%D9%83%D8%A7%D9%85%D9%84-%D9%8A%D9%86%D8%A7%D8%B3%D8%A8-%D9%85%D8%B4%D8%B1%D9%88%D8%B9%D9%83-%D9%88%D9%81%D9%83%D8%B1%D8%AA%D9%83" target="blank">Lets Do it</a></div></div>
       </div>
       <div class="w-[70%] flex flex-col mx-auto justify-center">
